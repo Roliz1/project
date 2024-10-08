fe66d2b8ac2349e61d6ce70176e6cac448704156
Roman <romika99@mail.ru>
2024-10-08 15:14:30 +0500

    Add gitignore

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const numberOfFilms = +promt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMovieDB = {
+     count: personalMovieDB, 
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('Насколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.actors[c] = d;
+
+console.log(personalMovieDB);
+
